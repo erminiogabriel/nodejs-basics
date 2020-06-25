7c9becfe6e2bb089c6cd477163b8e1835b61aff5
Gabriel Machado <gabriel@movitech.com.br>
2020-06-25 15:01:51 -0300

    Desenvolvimento da Api, adicionado o CRUD no banco de dados.

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 //ORM Mapeamento objeto-relacional
 const mongoose = require("mongoose");
 const requireDir = require("require-dir");
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
 mongoose.connect("mongodb://192.168.25.5:27017/nodeapi", {useNewUrlParser: true, useUnifiedTopology: true});
 requireDir("./src/models");
